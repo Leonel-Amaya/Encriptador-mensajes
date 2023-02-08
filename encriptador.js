86131591cd398ec3d6150602092034b146e4c3b2
Leonel Amaya <omar.mejia2099@gmail.com>
2023-02-07 19:58:13 -0500

    Creación función desencriptar en JS

--- encriptador.js
+++ encriptador.js
@@ -1,5 +1,3 @@
-
-
 function encriptar(){
     var mensaje = document.getElementById("texto").value.toLowerCase();
 
@@ -10,6 +8,27 @@ function encriptar(){
     mensajeEncriptado = mensajeEncriptado.replace(/u/igm,"ufat");
 
     document.getElementById("resultado").innerHTML = mensajeEncriptado;
+    document.getElementById("resultado").style.display="show";
+    document.getElementById("resultado").style.display="inherit";
+
+    document.getElementById("muneco").style.visibility="hidden";
+    document.getElementById("subtitulo").style.visibility="hidden";
+    document.getElementById("parrafo").style.visibility="hidden";
+
+    document.getElementById("copiarTexto").style.display="show";
+    document.getElementById("copiarTexto").style.display="inherit";
+}
+
+function desencriptar(){
+    var mensaje = document.getElementById("texto").value.toLowerCase();
+
+    let mensajeDesencriptado = mensaje.replace(/enter/igm,"e");
+    mensajeDesencriptado = mensajeDesencriptado.replace(/imes/igm,"i");
+    mensajeDesencriptado = mensajeDesencriptado.replace(/ai/igm,"a");
+    mensajeDesencriptado = mensajeDesencriptado.replace(/ober/igm,"o");
+    mensajeDesencriptado = mensajeDesencriptado.replace(/ufat/igm,"u");
+
+    document.getElementById("resultado").innerHTML = mensajeDesencriptado;
     document.getElementById("resultado").style.display="show";
     document.getElementById("resultado").style.display="inherit";
 
